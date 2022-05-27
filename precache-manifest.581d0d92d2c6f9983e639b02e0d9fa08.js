d253f71d5603b23af1d332fffdce06d1b0080933
Code Sandbox <hello@codesandbox.io>
2022-05-27 13:54:33 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.581d0d92d2c6f9983e639b02e0d9fa08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dab8c21175a0ce6ad3f758525a06dee0",
+    "url": "https://tte92.github.io/csb-gpt3ir/index.html"
+  },
+  {
+    "revision": "18301c576fb7de0f50bd",
+    "url": "https://tte92.github.io/csb-gpt3ir/static/css/main.81e5ac81.chunk.css"
+  },
+  {
+    "revision": "c9a8b0fa8a760bf9809d",
+    "url": "https://tte92.github.io/csb-gpt3ir/static/js/2.55c71b4c.chunk.js"
+  },
+  {
+    "revision": "18301c576fb7de0f50bd",
+    "url": "https://tte92.github.io/csb-gpt3ir/static/js/main.36f0ce66.chunk.js"
+  },
+  {
+    "revision": "e5228951e173783f0102",
+    "url": "https://tte92.github.io/csb-gpt3ir/static/js/runtime~main.013461c1.js"
+  }
+]);
